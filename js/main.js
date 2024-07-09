76611e1ddbc1a1e9c10db007b25c5d4a5bf0e647
Mostafa Hussein <mostdrsh54012@gmail.com>
2024-07-09 15:40:28 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const header = document.querySelector('#header')
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 100) {
+//       header.style.background = "";
+//       header.style.padding = "20px 0";
+//     } else {
+//       header.style.background = "#525A60;";
+//       header.style.padding = "60px 0";
+//     }
+//   });
